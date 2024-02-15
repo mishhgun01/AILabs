cac1b0764225ad8250d8d18d92c5765b5a701cdd
mishhgun01 <golubev1400@mail.ru>
2024-02-15 13:26:30 +0300

    position

--- lab1/pkg/alghorithm.go
+++ lab1/pkg/alghorithm.go
@@ -1,6 +1,15 @@
 package pkg
 
+type position string
+
+const (
+	LEFT   = "Left"
+	RIGHT  = "Right"
+	TOP    = "Top"
+	BOTTOM = "Bottom"
+)
+
 // Alghorithm - функция генерации последовательностей - обход в глубину, с итеративным углублением.
-func Alghorithm() []state {
+func Alghorithm(swapPosition position) []state {
 	return nil
 }
--- lab1/pkg/resolver.go
+++ lab1/pkg/resolver.go
@@ -1,7 +1,7 @@
 package pkg
 
 // resolver - шаблон функции решения задачи
-type resolver func() []state
+type resolver func(position) []state
 
 // Resolve - функция решающая задачу.
 func Resolve(state *state, generate resolver) []state {
