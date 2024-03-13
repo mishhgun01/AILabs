be322ae32ad945df0667359cbe7548901a693222
mishhgun01 <golubev1400@mail.ru>
2024-03-13 14:10:39 +0300

    menu

--- lab1/cmd/main.go
+++ lab1/cmd/main.go
@@ -13,15 +13,38 @@ import (
 
 func main() {
 
+	var algoChosen, stepsChosen int
+	var withSteps bool
+	fmt.Println("Выберите алгоритм:\n1 - В глубину\n2 - В глубину с итеративным углублением")
+	fmt.Print("->")
+	fmt.Scan(&algoChosen)
+	fmt.Println("Пошагово или автоматически:\n1 - Пошагово\n2 - Автоматически")
+	fmt.Print("->")
+	fmt.Scan(&stepsChosen)
+	if stepsChosen == 1 {
+		withSteps = true
+	}
 	file, err := os.OpenFile(consts.FILENAME, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	var algo func(startNode *node.Node, option bool) ([]node.Node, int, int)
+
+	switch algoChosen {
+	case 1:
+		algo = alghorithms.DFS
+	case 2:
+		algo = alghorithms.IDDFS
+	default:
+		fmt.Println("Неизвестный алгоритм")
+		return
+	}
+
 	startNode := node.NewNode(1, state.StartState(), node.DefaultDepthGenerator, nil)
 	start := time.Now()
-	output, steps, nodeCount := solver.Resolve(startNode, true, alghorithms.DFS)
+	output, steps, nodeCount := solver.Resolve(startNode, withSteps, algo)
 	duration := time.Since(start)
 	if output != nil {
 		for _, vertex := range output {
--- lab2/cmd/main.go
+++ lab2/cmd/main.go
@@ -12,15 +12,42 @@ import (
 )
 
 func main() {
+	var algoChosen, stepsChosen int
+	var withSteps bool
+	fmt.Println("Выберите алгоритм:\n1 - Жадный с h1\n2 - Жадный с h2\n3 - А* с h1\n4 - А* с h2")
+	fmt.Print("->")
+	fmt.Scan(&algoChosen)
+	fmt.Println("Пошагово или автоматически:\n1 - Пошагово\n2 - Автоматически")
+	fmt.Print("->")
+	fmt.Scan(&stepsChosen)
+	if stepsChosen == 1 {
+		withSteps = true
+	}
 	file, err := os.OpenFile(consts.FILENAME, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
 
-	startNode := node.NewNode(1, state.StartState(), node.ManhattanDistanceGenerator, nil)
+	var algo func(*node.Node)
+
+	defer file.Close()
+	switch algoChosen {
+	case 1:
+		algo = node.MinimumStepsDepthGenerator
+	case 2:
+		algo = node.ManhattanDistanceGenerator
+	case 3:
+		algo = node.AStarMinimumStepsDepthGenerator
+	case 4:
+		algo = node.AStarManhattanDistanceGenerator
+	default:
+		fmt.Println("Неизвестный алгоритм")
+		return
+	}
+
+	startNode := node.NewNode(1, state.StartState(), algo, nil)
 	start := time.Now()
-	output, steps, nodeCount := solver.Resolve(startNode, false, alghorithms.Search)
+	output, steps, nodeCount := solver.Resolve(startNode, withSteps, alghorithms.Search)
 
 	duration := time.Since(start)
 	if output != nil {
